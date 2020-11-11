2bf5b3fefd893cbb2a532b3c6608bff9f5822aa4
marius15ru <marius15@ru.is>
2020-11-11 14:26:34 +0000

    BTest projectm with Area tests

--- HEF-Test/AreaTests.cs
+++ HEF-Test/AreaTests.cs
@@ -1,10 +1,100 @@
 ﻿using System;
+using HEF_API.Models;
+using HEF_API.Controllers;
+using Xunit;
+using Moq;
+using System.Collections.Generic;
+using HEF_API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Net.Http;
+
 namespace HEF_Test
 {
-    public class AreaTests
+    public class AreaTests: TestWithSqlite
     {
-        public AreaTests()
+        private readonly ITestOutputHelper output;
+        private readonly AreaService _service;
+
+        public AreaTests(ITestOutputHelper output)
         {
+            this.output = output;
+            Populate();
+            _service = new AreaService(dbContext);
+        }
+
+        private void Populate()
+        {
+            dbContext.Add(new Area { Name = "Fellar" });
+            dbContext.Add(new Area { Name = "Egilsst" });
+            dbContext.SaveChanges();
+        }
+
+        [Fact]
+        public async Task GetAll()
+        {
+            var res = await _service.GetAllAreas();
+
+            var dbCount = res.Count();
+            var lastId = res.Last().Id;
+
+            Assert.Equal(2, dbCount);
+            Assert.Equal(2, lastId);
+        }
+
+        [Fact]
+        public async Task GetById()
+        {
+            var id = 1;
+            var res = await _service.GetAreaById(id);
+
+            var redId = res.Id;
+
+            Assert.Equal(1, redId);
+        }
+
+        [Fact]
+        public async Task Create()
+        {
+            var enitity = new Area { Name = "Vogur" };
+
+            await _service.AddArea(enitity);
+
+            var dbCount = dbContext.Area.Count();
+            var lastId = dbContext.Area.OrderBy(x => x.Id).Last().Id;
+
+            Assert.Equal(3, dbCount);
+            Assert.Equal(3, lastId);
+        }
+
+        [Fact]
+        public async Task Update()
+        {
+            int id = 2;
+            var enitity = new Area { Name = "Vogur" };
+
+            await _service.EditArea(id, enitity);
+
+            var res = dbContext.Area.Where(x => x.Id == 2).Single().Name;
+
+            Assert.Equal("Vogur", res);
+        }
+
+        [Fact]
+        public async Task Delete()
+        {
+            int id = 2;
+            var before = dbContext.Area.Count();
+
+            await _service.RemoveArea(id);
+
+            var after = dbContext.Area.Count() + 1;
+
+            Assert.Equal(before, after);
         }
     }
 }
--- HEF-Test/dbTests.cs
+++ HEF-Test/dbTests.cs
@@ -1,10 +1,58 @@
 ﻿using System;
+using System.Threading.Tasks;
+using HEF_API.Models;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using System.Linq;
+
 namespace HEF_Test
 {
-    public class dbTests
+    public class DbContextTests : TestWithSqlite
     {
-        public dbTests()
+        [Fact]
+        public async Task DatabaseIsAvailableAndCanBeConnectedTo()
         {
+            Assert.True(await dbContext.Database.CanConnectAsync());
+        }
+    }
+
+    public class ItemConfigurationTests : TestWithSqlite
+    {
+        [Fact]
+        public void TableShouldGetCreated()
+        {
+            Assert.False(dbContext.Area.Any());
+        }
+
+        [Fact]
+        public void NameIsRequired()
+        {
+            var newItem = new Area();
+            dbContext.Area.Add(newItem);
+
+            //Assert.Throws<DbUpdateException>(() => dbContext.SaveChanges());
+        }
+
+        [Fact]
+        public void AddedItemShouldGetGeneratedId()
+        {
+            var newItem = new Area() { Name = "Testitem" };
+            dbContext.Area.Add(newItem);
+            dbContext.SaveChanges();
+
+            Assert.NotEqual(0, newItem.Id);
+        }
+
+        [Fact]
+        public void AddedItemShouldGetPersisted()
+        {
+            var newItem = new Area() { Name = "Testitem" };
+            dbContext.Area.Add(newItem);
+            dbContext.SaveChanges();
+
+            Assert.Equal(newItem, dbContext.Area.Find(newItem.Id));
+            Assert.Equal(1, dbContext.Area.Count());
         }
     }
 }
