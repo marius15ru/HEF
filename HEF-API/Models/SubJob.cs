3058aec610836650fa6b6be173a3a71d3bd50b47
marius15ru <marius15@ru.is>
2021-01-15 16:34:15 +0000

    Lagaði subjob

--- HEF-API/Models/SubJob.cs
+++ HEF-API/Models/SubJob.cs
@@ -7,10 +7,9 @@
         public int JobId { get; set; }
         public int EquipmentId { get; set; }
 
-        public string Name { get; set; }
         public string Description { get; set; }
         public int Status { get; set; }
-        public string Unit { get; set; }
+        public int Unit { get; set; }
         public double Value { get; set; }
         public int SubJobTask { get; set; }
     }
